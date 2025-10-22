a8491ffd4f576973f9bb460132e1c9df1a93698e
KrovavyElf <krovavyelfbw@gmail.com>
2025-10-22 18:59:56 +0300

    useritems

---
+++ Gymify.Web/Controllers/UserItemsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Gymify.Application.Services.Interfaces;
+
+namespace Gymify.Web.Controllers
+{
+    public class UserItemsController : Controller
+    {
+        private readonly IItemService _itemService;
+
+        public UserItemsController(IItemService itemService)
+        {
+            _itemService = itemService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var userId = Guid.Parse(User.FindFirst("UserProfileId")!.Value);
+            var items = await _itemService.GetAllUserItemsAsync(userId);
+            return View("UserItems", items);
+        }
+    }
+}
