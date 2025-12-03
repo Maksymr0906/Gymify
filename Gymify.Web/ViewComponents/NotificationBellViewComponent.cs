76359ba5b11366d32b33768866aa729dc3e2efe2
Nik0_olay <annaksota361@gmail.com>
2025-12-04 01:14:31 +0200

    Fixed localization in notification bell

--- Gymify.Web/ViewComponents/NotificationBellViewComponent.cs
+++ Gymify.Web/ViewComponents/NotificationBellViewComponent.cs
@@ -2,6 +2,7 @@
 using Gymify.Application.ViewModels.NotificationBell;
 using Gymify.Data.Interfaces.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
 using System.Security.Claims;
 
 namespace Gymify.Web.ViewComponents
@@ -10,6 +11,7 @@ namespace Gymify.Web.ViewComponents
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly INotificationService _notificationService;
+        private bool IsUkrainian => CultureInfo.CurrentCulture.Name == "uk-UA" || CultureInfo.CurrentCulture.TwoLetterISOLanguageName == "uk";
 
         public NotificationBellViewComponent(IUnitOfWork unitOfWork, INotificationService notificationService)
         {
@@ -20,7 +22,6 @@ namespace Gymify.Web.ViewComponents
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var userIdString = HttpContext.User.FindFirst("UserProfileId")?.Value;
-            bool ukraineVer = true;////////////////////////////////////////////////////////
 
             if (string.IsNullOrEmpty(userIdString))
                 return Content(""); 
@@ -28,13 +29,13 @@ namespace Gymify.Web.ViewComponents
             var userId = Guid.Parse(userIdString);
 
             var unreadCount = await _unitOfWork.NotificationRepository.GetUnreadCountAsync(userId);
-            var recentNotifications = await _notificationService.GetNotificationsAsync(userId, 20, ukraineVer);
+            var recentNotifications = await _notificationService.GetNotificationsAsync(userId, 20, IsUkrainian);
 
             var model = new NotificationBellViewModel
             {
                 UnreadCount = unreadCount,
                 Notifications = recentNotifications,
-                UkranianVer = ukraineVer
+                UkranianVer = IsUkrainian
             };
 
             return View(model);
